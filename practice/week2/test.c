ad65b5f32860683a70505fe9d2ef3fa5b3e492f6
handonghoo <handonghoo0812@gmail.com>
2023-09-12 16:42:19 +0900

    commit for test

---
+++ practice/week2/test.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int a = 10;
+    int b = 15;
+    printf("%d\n", a + b);
+
+    char text1[10] = "Hello";
+    char text2[50] = "World";
+
+    printf("%s!\n", text1);
+    printf("%s!\n", text2);
+
+    return 0;
+}
