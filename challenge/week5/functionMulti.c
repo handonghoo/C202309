43e86e23ca5c52e14f396222ee84b7d2ea1afb2b
handonghoo <handonghoo0812@gmail.com>
2023-10-07 13:43:53 +0900

    issue solved 1007

--- challenge/week5/functionMulti.c
+++ challenge/week5/functionMulti.c
@@ -3,9 +3,9 @@
 long multif(long value);
 // 함수를 이용해 변수를 넣어 출력합니다. 
 void main() {
-    printf("1부터 2까지의 곱은 %d\n", multif(2));
-    printf("1부터 3까지의 곱은 %d\n", multif(3));
-    printf("1부터 5까지의 곱은 %d\n", multif(5));
+    printf("1부터 2까지의 곱은 %ld\n", multif(2));
+    printf("1부터 3까지의 곱은 %ld\n", multif(3));
+    printf("1부터 5까지의 곱은 %ld\n", multif(5));
 }
 // 함수를 정의합니다. 
 long multif(long value)
