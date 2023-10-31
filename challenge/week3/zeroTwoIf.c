420dc590671b70cac812b32d43ba539b4f167e92
handonghoo <handonghoo0812@gmail.com>
2023-10-31 21:59:53 +0900

    file name and zeroTwolf error solved

--- challenge/week3/zeroTwoIf.c
+++ challenge/week3/zeroTwoIf.c
@@ -6,11 +6,11 @@ int main()
 	printf("숫자를 입력하시오: ");
 	scanf_s("%d", &num);
 	// 입력받은 숫자에 따라 조건문을 통해 다른 값을 출력합니다. 
-	if (num = 0)
+	if (num == 0)
 		printf("zero");
-	else if (num = 1)
+	else if (num == 1)
 		printf("one");
-	else if (num = 2)
+	else if (num == 2)
 		printf("two");
 	else
 		printf("not 0 ~ 2");
